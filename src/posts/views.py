3f977cbbbaba08fe31347c8684406947d794bbf9
eyoung8 <eyoung8@binghamton.edu>
2016-11-18 00:34:25 -0500

    Added images to blog posts and changed urls to be generated from title slugs

--- src/posts/views.py
+++ src/posts/views.py
@@ -10,7 +10,7 @@ from .models import Post
 
 
 def post_create(request):
-    form = PostForm(request.POST or None)
+    form = PostForm(request.POST or None, request.FILES or None)
     if form.is_valid():
         instance = form.save(commit=False)
         instance.save()
@@ -22,8 +22,8 @@ def post_create(request):
     }
     return render(request, "post_form.html", context)
 
-def post_detail(request,id):
-    instance = get_object_or_404(Post, id=id)
+def post_detail(request, slug):
+    instance = get_object_or_404(Post, slug=slug)
     context = {
         "title": instance.title,
         "instance": instance
@@ -50,9 +50,9 @@ def post_list(request):
     }
     return render(request, "post_list.html", context)
 
-def post_update(request, id=None):
-    instance = get_object_or_404(Post, id=id)
-    form = PostForm(request.POST or None, instance=instance)
+def post_update(request, slug=None):
+    instance = get_object_or_404(Post, slug=slug)
+    form = PostForm(request.POST or None, request.FILES or None, instance=instance)
     if form.is_valid():
         instance = form.save(commit=False)
         instance.save()
@@ -66,8 +66,8 @@ def post_update(request, id=None):
     }
     return render(request, "post_form.html", context)
 
-def post_delete(request, id=None):
-    instance = get_object_or_404(Post, id=id)
+def post_delete(request, slug=None):
+    instance = get_object_or_404(Post, slug=slug)
     instance.delete()
     messages.success(request, "Post Deleted")
     return redirect("posts:list")
