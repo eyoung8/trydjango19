3300cba5872e2d5c4fd0ff847dbe4ace4b23ff44
eyoung8 <eyoung8@binghamton.edu>
2016-11-21 02:20:13 -0500

    Added markdown filter to text, first with jQuery then with the django app markdown_deux. Used the django app pagedown to add a stackoverflow style markdown editor to the blog editor. Used jQuery to add img-responsive tags to any div class='post-detail-item'.

--- src/posts/models.py
+++ src/posts/models.py
@@ -4,6 +4,8 @@ from django.db import models
 from django.db.models.signals import pre_save
 from django.utils import timezone
 from django.utils.text import slugify
+from django.utils.safestring import mark_safe
+from markdown_deux import markdown
 # Create your models here.
 
 class PostManager(models.Manager):
@@ -35,6 +37,11 @@ class Post(models.Model):
 
     def get_absolute_url(self):
         return reverse("posts:detail", kwargs = {"slug": self.slug})
+
+    def get_markdown(self):
+        content = self.content
+        markdown_text = markdown(content)
+        return mark_safe(markdown_text)
 
     def __str__(self):
         return self.title
